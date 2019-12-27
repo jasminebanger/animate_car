95a811f4b06f5a45d731cb703dea87334b6f53db
jasminebanger <37240370+jasminebanger@users.noreply.github.com>
2019-12-27 16:54:24 +0530

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,4 +1,7 @@
 // js code for animate car
 $(document).ready(function(){
   $surface = $('.surface');
+  $(document).on('keypress',function(e){
+                if(e.which == 13){
+                    $($surface).toggleClass('moveRight');
 });
