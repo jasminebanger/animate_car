a41faeeab5b78a0cda8d8b6b116b2d8a8daa91b1
jasminebanger <37240370+jasminebanger@users.noreply.github.com>
2019-12-27 16:55:56 +0530

    Update magic.js

--- magic.js
+++ magic.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   //variable
   $surface = $('.surface');
+  //keypress events
   $(document).on('keypress',function(e){
                 if(e.which == 13){
                     $($surface).toggleClass('moveRight');
